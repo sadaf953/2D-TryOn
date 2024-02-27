4e1ddbcac26d05324c54a1cf60fd5d031ffb4ae6
Mahvish Sadaf <mahvishsadaf953@yahoo.com>
2024-02-27 16:45:14 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,143 @@
+import os
+import cvzone
+import cv2
+import math
+import mediapipe as mp
+
+class PoseDetector:
+    def __init__(self, staticMode=False,
+                 modelComplexity=1,
+                 smoothLandmarks=True,
+                 enableSegmentation=False,
+                 smoothSegmentation=True,
+                 detectionCon=0.5,
+                 trackCon=0.5):
+        self.staticMode = staticMode
+        self.modelComplexity = modelComplexity
+        self.smoothLandmarks = smoothLandmarks
+        self.enableSegmentation = enableSegmentation
+        self.smoothSegmentation = smoothSegmentation
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(static_image_mode=self.staticMode,
+                                     model_complexity=self.modelComplexity,
+                                     smooth_landmarks=self.smoothLandmarks,
+                                     enable_segmentation=self.enableSegmentation,
+                                     smooth_segmentation=self.smoothSegmentation,
+                                     min_detection_confidence=self.detectionCon,
+                                     min_tracking_confidence=self.trackCon)
+
+    def findPose(self, img, draw=True):
+        if img is None:
+            print("Error: Empty frame received.")
+            return None
+
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            pass  # Remove drawing command
+        return img
+
+    def findPosition(self, img, draw=True, bboxWithHands=False):
+        self.lmList = []
+        self.bboxInfo = {}
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy, cz = int(lm.x * w), int(lm.y * h), int(lm.z * w)
+                self.lmList.append([cx, cy, cz])
+
+        return self.lmList, self.bboxInfo
+
+
+    def findDistance(self, p1, p2, img=None, color=(255, 0, 255), scale=5):
+        x1, y1 = p1
+        x2, y2 = p2
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        length = math.hypot(x2 - x1, y2 - y1)
+        info = (x1, y1, x2, y2, cx, cy)
+        return length, img, info
+
+    def findAngle(self, p1, p2, p3, img=None, color=(255, 0, 255), scale=5):
+        # Get the landmarks
+        x1, y1 = p1
+        x2, y2 = p2
+        x3, y3 = p3
+
+        # Calculate the Angle
+        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
+                             math.atan2(y1 - y2, x1 - x2))
+        if angle < 0:
+            angle += 360
+
+
+    def angleCheck(self, myAngle, targetAngle, offset=20):
+        return targetAngle - offset < myAngle < targetAngle + offset
+
+def main():
+    cap = cv2.VideoCapture("Resources/Videos/1.mp4")
+    detector = PoseDetector()
+    shirtFolderPath = "Resources/Shirts"
+    listShirts = os.listdir(shirtFolderPath)
+    fixedRatio = 262 / 190  # widthOfShirt/widthOfPoint11to12
+    shirtRatioHeightWidth = 581 / 440
+    imageNumber = 0
+    imgButtonRight = cv2.imread("Resources/button.png", cv2.IMREAD_UNCHANGED)
+    imgButtonLeft = cv2.flip(imgButtonRight, 1)
+    counterRight = 0
+    counterLeft = 0
+    selectionSpeed = 10
+
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False, draw=False)
+        if lmList:
+            lm11 = lmList[11][0:2]
+            lm12 = lmList[12][0:2]
+            imgShirt = cv2.imread(os.path.join(shirtFolderPath, listShirts[imageNumber]), cv2.IMREAD_UNCHANGED)
+
+            widthOfShirt = int((lm11[0] - lm12[0]) * fixedRatio)
+            if widthOfShirt <= 0:
+                print("Error: Invalid width of shirt:", widthOfShirt)
+                continue
+
+            imgShirt = cv2.resize(imgShirt, (widthOfShirt, int(widthOfShirt * shirtRatioHeightWidth)))
+            currentScale = (lm11[0] - lm12[0]) / 190
+            offset = int(44 * currentScale), int(48 * currentScale)
+
+            try:
+                img = cvzone.overlayPNG(img, imgShirt, (lm12[0] - offset[0], lm12[1] - offset[1]))
+            except:
+                pass
+
+            img = cvzone.overlayPNG(img, imgButtonRight, (1074, 293))
+            img = cvzone.overlayPNG(img, imgButtonLeft, (72, 293))
+
+            if lmList[16][1] < 400:  # Adjusted threshold for right button
+                counterRight += 1
+                cv2.ellipse(img, (139, 360), (66, 66), 0, 0,
+                            counterRight * selectionSpeed, (0, 255, 0), 20)
+                if counterRight * selectionSpeed > 360:
+                    counterRight = 0
+                    if imageNumber < len(listShirts) - 1:
+                        imageNumber += 1
+            elif lmList[15][1]>650:  # Adjusted threshold for left button
+                counterLeft += 1
+                cv2.ellipse(img, (1138, 360), (66, 66), 0, 0,
+                            counterLeft * selectionSpeed, (0, 255, 0), 20)
+                if counterLeft * selectionSpeed > 360:
+                    counterLeft = 0
+                    if imageNumber > 0:
+                        imageNumber -= 1
+
+            else:
+                counterRight = 0
+                counterLeft = 0
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
